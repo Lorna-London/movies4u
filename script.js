434b3b3a09451af4b21287f5c0ab0774e892c013
Lorna London <lornalondon109@hotmail.co.uk>
2024-02-14 14:35:15 +0000

    semantic elements added to app

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
 const form = document.querySelector('#form');
+const searchBox = document.querySelector('search-box');
 const searchButton = document.querySelector('#search-btn');
 const deleteButton = document.querySelector('#delete-btn');
-const searchBox = document.querySelector('search-box');
+const imagesContainer = document.querySelector('#images-container');
+const body = document.querySelector('#body');
 
-form.addEventListener('submit', function (e) {
+
+form.addEventListener('submit', async function (e) {
     e.preventDefault();
+    getMovies();
 })
 
 const getMovies = async function () {
     try {
         const searchTerm = form.elements.query.value;
-        const config = {headers: {Accept: 'application/json'}}
-        const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}, config`);
-        console.log('Resolved!', res);
-        console.log(res.data);
+        const config = {params: {q: searchTerm}, headers: {Accept: 'application/json'}}
+        const res = await axios.get('https://api.tvmaze.com/search/shows/', config); 
+        makeImages(res.data);  
+        form.elements.query.value = '';
     }
     catch (e) {
         console.log('Rejected! Error', e);
     }
 }
 
-const makeImages = function() {
-//https://api.tvmaze.com/shows/1/images
+const makeImages = (shows) => {
+for(let result of shows) {
+    if(result.show.image) {
+        const img = document.createElement('img');
+        img.src = result.show.image.medium;   
+        imagesContainer.append(img);  
+        img.style.borderRadius = '12px';
+        } 
+      } 
+    }
 
+ const deleteImages = function () {
+    imagesContainer.classList.add('hide');
 }
 
-//data.show.image.medium
+deleteButton.addEventListener('click', deleteImages);
